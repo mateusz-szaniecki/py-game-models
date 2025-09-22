d2958c6d943445b897c7d3ea34d1661345dca2ff
Mateusz <szanieckimateusz@gmail.com>
2025-09-22 13:43:14 +0200

    Implement guild assignments for database

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
 import init_django_orm  # noqa: F401
+import json
 
 from db.models import Race, Skill, Player, Guild
 
 
 def main() -> None:
-    pass
+    with open("players.json", "r") as file:
+        players = json.load(file)
+        for player_data in players.values():
+            guild_data = player_data["guild"]
+
+            if guild_data:
+                Guild.objects.get_or_create(
+                    name=player_data["guild"]["name"],
+                    description=player_data["guild"]["description"],
+                )
 
 
 if __name__ == "__main__":

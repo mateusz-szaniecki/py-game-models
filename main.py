157af90e19ec49ce3b8acc78074f5834bd483766
Mateusz <szanieckimateusz@gmail.com>
2025-09-22 14:29:51 +0200

    Complete implementation of main.py assignment

--- main.py
+++ main.py
@@ -16,6 +16,37 @@ def main() -> None:
                     description=player_data["guild"]["description"],
                 )
 
+            race, _ = Race.objects.get_or_create(
+                name=player_data["race"]["name"],
+                defaults={"description": player_data["race"]["description"]}
+            )
+
+            for skill_data in player_data["race"]["skills"]:
+                Skill.objects.get_or_create(
+                    name=skill_data["name"],
+                    race=race,
+                    defaults={
+                        "bonus": skill_data["bonus"],
+                    }
+                )
+
+        for player_name, player_data in players.items():
+            race = Race.objects.get(name=player_data["race"]["name"])
+
+            guild = None
+            if player_data["guild"]:
+                guild = Guild.objects.get(name=player_data["guild"]["name"])
+
+            Player.objects.get_or_create(
+                email=player_data["email"],
+                defaults={
+                    "nickname": player_name,
+                    "race": race,
+                    "guild": guild,
+                    "bio": player_data["bio"],
+                }
+            )
+
 
 if __name__ == "__main__":
     main()
